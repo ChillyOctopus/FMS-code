122cc1ff26c803d23abdd7652ad7c7c1bd607daf
ChillyOctopus <jistevenson01@gmail.com>
2023-02-15 14:30:09 -0700

    Base setup, classes and directories

---
+++ src/main/java/Models/Person.java
@@ -0,0 +1,67 @@
+package Models;
+
+public class Person {
+    private String personID;
+    private String associatedUsername;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String fatherID;
+
+    public Person(String personID, String associatedUsername, String firstName, String lastName, String gender, String fatherID) {
+        this.personID = personID;
+        this.associatedUsername = associatedUsername;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.fatherID = fatherID;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+    public String getAssociatedUsername() {
+        return associatedUsername;
+    }
+
+    public void setAssociatedUsername(String associatedUsername) {
+        this.associatedUsername = associatedUsername;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFatherID() {
+        return fatherID;
+    }
+
+    public void setFatherID(String fatherID) {
+        this.fatherID = fatherID;
+    }
+}
---
+++ src/main/java/Req_Res/ErrorResponse.java
@@ -0,0 +1,7 @@
+package Req_Res;
+
+public class ErrorResponse {
+    String message;
+    String success;
+
+}
