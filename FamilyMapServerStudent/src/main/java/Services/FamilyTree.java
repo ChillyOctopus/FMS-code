a513b5307d91ac20c5741bed19590f0f075c9870
ChillyOctopus <111397038+ChillyOctopus@users.noreply.github.com>
2023-02-15 20:01:10 -0700

    Add files via upload

--- FamilyMapServerStudent/src/main/java/Services/FamilyTree.java
+++ FamilyMapServerStudent/src/main/java/Services/FamilyTree.java
@@ -1,4 +1,39 @@
 package Services;
 
+import Models.Person;
+
+import java.util.List;
+
 public class FamilyTree {
+
+    /**
+     * returns a list of all a persons past relatives
+     * @param personID the persons (user or regular) whose relatives we return
+     * @return a list of the persons relatives based on ID
+     */
+    public List<Person> relatives(String personID){
+        List<Person> myList = null;
+        return myList;
+    }
+
+    /**
+     * Function to find paternal line of person (user or regular)
+     * @param personID the person in question
+     * @return a list of Person objects, father->grandfather->great ect
+     */
+    public List<Person> paternalLine(String personID){
+        List<Person> myList = null;
+        return myList;
+    }
+
+    /**
+     * Function to find maternal line of person (user or regular)
+     * @param personID the person in question
+     * @return a list of Person objects, mother->grandmother->great ect
+     */
+    public List<Person> maternalLine(String personID){
+        List<Person> myList = null;
+        return myList;
+    }
+
 }
